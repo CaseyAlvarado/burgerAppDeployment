f9c82febf235c7f99379dbafc4dc312f21cb3caa
Casey Alvarado <alvarado.casey@gmail.com>
2016-02-23 17:10:10 -0500

    marp

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/ingredients', ingredients.getIngredientsGET);
 app.post('/ingredients/add', ingredients.addIngredientPOST);
 app.post('/ingredients/edit', ingredients.updateIngredientsPOST); 
