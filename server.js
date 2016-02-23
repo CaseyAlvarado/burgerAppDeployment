8f6f5d31f7359c5b003d7abda41bb2caf86fc660
Casey Alvarado <alvarado.casey@gmail.com>
2016-02-23 17:07:52 -0500

    changed mongo db config name to PROD_MONGODB

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.MONGOCONN);
+mongoose.connect(process.env.PROD_MONGODB);
 
 var express = require('express');
 var ingredients = require('./routes/ingredients');
